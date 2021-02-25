4e2377f7c912d51140c2fbee5cdd390b56321bd9
galamshar <myrzagalyev.galymzhan@gmail.com>
2021-02-25 23:10:06 +0600

    Added web pages

---
+++ src/ShoppingWeb/ApiContainer/IOrderApi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingWeb.ApiContainer
+{
+    public interface IOrderApi
+    {
+
+    }
+}
